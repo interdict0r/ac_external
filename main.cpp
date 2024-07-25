4620f4ab347477a2be11a7170e534763d28cf13e
alp <76008113+interdict0r@users.noreply.github.com>
2024-07-25 16:26:48 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include "utilities/memory.h";
+#include <iostream>
+#include <vector>
+#include <thread>
+
+const std::string red = "\033[31m";
+const std::string green = "\033[32m";
+const std::string yellow = "\033[33m";
+const std::string blue = "\033[34m";
+const std::string magenta = "\033[35m";
+const std::string cyan = "\033[36m";
+const std::string white = "\033[37m";
+const std::string reset = "\033[0m";
+
+// 1.2.0.2 ac offsets
+constexpr auto localPlayer = 0x10F4F4;
+constexpr auto health = 0xF8;
+constexpr auto armor = 0xFC;
+constexpr auto mtpAmmo = 0x150;
+constexpr auto nameOffsetWithinPadding = 0x0225;
+
+// ascii logo
+std::vector<std::string> logo{
+	"            _ _ _            _     ",
+	" __   _____(_) | |_ ___  ___| |__  ",
+	" \\ \\ / / _ \\ | | __/ _ \\/ __| '_ \\ ",
+	"  \\ V /  __/ | | ||  __/ (__| | | |",
+	"   \\_/ \\___|_|_|\\__\\___|\\___|_| |_|",
+	"                                   "
+};
+
+class NamePadding
+{
+public:
+	char preNamePadding_0000[548]; //0x0000
+	char maxAcceptableCharacters_0224[16]; //0x0224
+	char postNamePadding_0234[3608]; //0x0234
+}; //Size: 0x104C
+
+int main()
+{
+	Memory memory{ "ac_client.exe" };
+	const auto ModuleBaseAddress = memory.GetModuleAddress("ac_client.exe");
+
+	// dereference the pointer
+	const auto localPlayerAddress = memory.Read<std::uintptr_t>(ModuleBaseAddress + localPlayer);
+	const auto healthAddress = localPlayerAddress + health;
+	const auto armorAddress = localPlayerAddress + armor;
+	const auto mtpAmmoAddress = localPlayerAddress + mtpAmmo;
+	const auto nameAddress = localPlayerAddress + nameOffsetWithinPadding;
+
+	std::string name = memory.Read<NamePadding>(nameAddress).preNamePadding_0000;
+
+	std::cout << std::endl;
+	for (const auto& line : logo)
+	{
+		std::cout << cyan << line << std::endl;
+	}
+
+	std::cout << std::endl << yellow << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << reset << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "entry confirmed, " << magenta << name << "\n" << reset << green << std::endl;
+	std::cout << green << "f1 : max health " << std::endl;
+	std::cout << green << "f2 : max armor " << std::endl;
+	std::cout << green << "f1 : max ammo  " << std::endl;
+
+	while (true)
+	{
+		//if (GetAsyncKeyState(VK_F1) & 0x8000)
+		//	memory.Write<int>(healthAddress, 999999);
+		//else if (GetAsyncKeyState(VK_F2) & 0x8000)
+		//	memory.Write<int>(armorAddress, 999999);
+		//else if (GetAsyncKeyState(VK_F3) & 0x8000)
+		//	memory.Write<int>(mtpAmmoAddress, 999999);
+
+		if (GetAsyncKeyState(VK_F1) & 0x8000)
+		{
+			memory.Write<int>(healthAddress, 999999);
+			std::cout << "health changed" << std::endl;
+		}
+		else if (GetAsyncKeyState(VK_F2) & 0x8000)
+		{
+			memory.Write<int>(armorAddress, 999999);
+			std::cout << "armor changed" << std::endl;
+		}
+		else if (GetAsyncKeyState(VK_F3) & 0x8000)
+		{
+			memory.Write<int>(mtpAmmoAddress, 999999);
+			std::cout << "ammo changed" << std::endl;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
---
+++ utilities/memory.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <TlHelp32.h>
+
+#include <cstdint>
+#include <string_view>
+
+class Memory
+{
+private:
+	std::uintptr_t processId = 0;
+	void* processHandle = nullptr;
+
+public:
+	// fuck you windows
+	Memory(const std::string_view processName) noexcept 
+	{
+		::PROCESSENTRY32 entry = { };
+		entry.dwSize = sizeof(::PROCESSENTRY32);
+
+		// create snapshot of running processes
+		const auto processSnapshot = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+		// find right process
+		while (::Process32Next(processSnapshot, &entry))
+		{
+			if (!processName.compare(entry.szExeFile))
+			{
+				processId = entry.th32ProcessID;
+				processHandle = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+				break;
+			}
+		}
+
+		// close handle
+		if (processSnapshot)
+			::CloseHandle(processSnapshot);
+	}
+
+	~Memory()
+	{
+		if (processHandle)
+			::CloseHandle(processHandle);
+	}
+
+	const std::uintptr_t GetModuleAddress(const std::string_view moduleName) const noexcept
+	{
+		::MODULEENTRY32 entry = { };
+		entry.dwSize = sizeof(::MODULEENTRY32);
+
+		const auto processSnapshot = ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPPROCESS, processId);
+		std::uintptr_t moduleBaseAddress = 0;
+
+		while (::Module32Next(processSnapshot, &entry))
+		{
+			if (!moduleName.compare(entry.szModule))
+			{
+				// preferred imagebase
+				moduleBaseAddress = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+				break;
+			}
+		}
+
+		if (processSnapshot)
+			::CloseHandle(processSnapshot);
+
+		return moduleBaseAddress;
+	}
+
+	template <typename T>
+	constexpr const T Read(const std::uintptr_t& address) const noexcept
+	{
+		T value = { };
+		::ReadProcessMemory(processHandle, reinterpret_cast<const void*>(address), &value, sizeof(T), NULL);
+		return value;
+	}
+
+	template <typename T>
+	constexpr void Write(const std::uintptr_t& address, const T& value) const noexcept
+	{
+		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
+	}
+};
